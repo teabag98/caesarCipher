75b12550e5a33b01a37e6b0bb51e9d42325335c2
eric <eric.kingoo2014@gmail.com>
2019-04-08 10:35:10 +0300

    check if input is a string

--- src/main/java/CaesarCipher.java
+++ src/main/java/CaesarCipher.java
@@ -4,15 +4,37 @@
 public class CaesarCipher{
 
    String[] alphabets = {A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z};
-    public String runCaesarCipher(String InputtedMessage){
-     boolean filterNumbers = InputtedMessage.matches("[*0-9]");
-//
-        if(!filterNumbers){
-         char[] eachLetterSplitted  = InputtedMessage.toCharArray();
-         for(int i=1; i<=eachLetterSplitted.length; i++ ){
-            if() {}
+    public String runCaesarCipher(String InputtedMessage,int shiftBy){
+//     boolean filterNumbers = InputtedMessage.matches("[*0-9]");
+////
+//        if(!filterNumbers){
+//         char[] eachLetterSplitted  = InputtedMessage.toCharArray();
+////   }
+
+  if(shiftBy > 26){
+     shiftBy = shiftBy % 26;
+  }
+  else if(shiftBy<26){
+      shiftBy = (shiftBy%26)+26;
+  }
+    String encryptedText = "";
+   int lengthOfInput = InputtedMessage.length();
+    for (int i=0; i<lengthOfInput; i++){
+       char eachCharacter = InputtedMessage.charAt(i);
+       if(Character.isLetter(eachCharacter)){
+        char shiftedCharacter = (char)(eachCharacter+shiftBy);
+         if(shiftedCharacter >'z'){
+             encryptedText += (char)(shiftedCharacter-(26-shiftBy));
+
          }
-        }
+         else()
+
+       }
+       else {
+     encryptedText += eachCharacter;
+       }
+    }
+     return encryptedText;
     }
 
 }
